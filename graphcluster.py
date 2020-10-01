34288eee712551dfbe6e320ae5879c317c86fd14
Brendan Cook <brendan1cook@gmail.com>
2020-09-30 19:15:01 -0500

    Implemented fixes to poisson_cluster_kmeans. Added experiment() function and several experiments in notebook.ipynb.

--- graphcluster.py
+++ graphcluster.py
@@ -1,16 +1,18 @@
 '''
-Graph Clustering methods
+Graph Clustering Algorithms
 Implemented by Brendan Cook
 '''
 
 import numpy as np
-import GraphLearning.graphlearning as gl
+import graphlearning as gl
 import matplotlib.pyplot as plt
 import sklearn.datasets as datasets
 from sklearn.cluster import KMeans
+import sklearn
 import seaborn as sns
 import kmeans1d
 import scipy
+import ipdb
 
 # To-do: Finish poisson_topdown
 # Test poisson_kmeans
@@ -30,59 +32,65 @@ def gaussian_mixture():
     X = np.vstack((X1,X2,X3))
     L = np.hstack((L1,L2,L3))
     return X, L
-def moons_dataset(n=500, noise=0.1):
-    X, L = datasets.make_moons(n_samples=n, noise=noise)
-    return X, L
-def wines_dataset():
-    X, L = load_wine(return_X_y=True)
-    return X, L
 def build_graph(X, k=10):
     #Build graph
     I,J,D = gl.knnsearch(X,k)
     W = gl.weight_matrix(I,J,D,k)
     W = gl.diag_multiply(W,0)
+    while not gl.isconnected(W):
+        k += 5
+        I,J,D = gl.knnsearch(X,k)
+        W = gl.weight_matrix(I,J,D,k)
+        W = gl.diag_multiply(W,0)
     return W  
-def poisson_cluster_kmeans(X, W, labels, k, centroid_method='closest', max_iter=20, num_starts=1, show_plot=True):
+
+### Poisson K-means ###
+
+def label_matrix(y):
+    U, _ = gl.LabelsToVec(np.asarray(y))
+    return U.transpose()
+def update_centroids(W, L, y_pred, indices, n, k, centroid_method):
+    if centroid_method == 'closest':
+        m = [np.mean(X[y_pred == j, :] ,axis=0) for j in range(k)]
+        centroids = [np.argmin(np.linalg.norm(X - m[j], axis=1)) for j in range(k)]
+    elif centroid_method == 'laplacian_eigenvector':
+        #eigenvectors = np.vstack((gl.dirichlet_eigenvectors(L, indices[y_pred != i] , 1)[0] for i in range(k)))
+        eigenvectors, _ = gl.dirichlet_eigenvectors(L, I=indices[y_pred != 0] , k=1)
+        for i in range(1,k):
+            new_eigenvector, _ = gl.dirichlet_eigenvectors(L, I=indices[y_pred != i] , k=1)
+            eigenvectors = np.vstack((eigenvectors, new_eigenvector))
+        centroids = np.argmax(np.abs(eigenvectors), axis=1)
+    elif centroid_method == 'poisson_solve':
+        u_vals = [gl.constrained_solve(L, I=indices[y_pred != i], g=np.zeros(len(indices[y_pred != i])), f=np.ones(n)) for i in range(k)]
+        centroids = [np.argmax(np.abs(u)) for u in u_vals]
+    elif centroid_method == 'dijkstra':
+        u_vals = [gl.cDijkstra(W, I=indices[y_pred != i], g=np.zeros(len(indices[y_pred != i]))) for i in range(k)]
+        centroids = [np.argmax(u) for u in u_vals]
+    else:
+        raise Exception('Invalid Centroid Method')
+    return centroids
+def poisson_cluster_kmeans(W, y, L, k, centroid_method='dijkstra', centroid_inits=None, max_iter=20, num_starts=1, show_plot=False):
     #Randomly choose k labeled points
     n = W.shape[0]
     L = gl.graph_laplacian(W)
     indices = np.arange(n)
-    centroids = np.random.choice(range(n),size=k,replace=False)
-    if show_plot: plt.ion()
-    last_acc = 100 
-    for i in range(max_iter):
-        #Semi-supervised learning 
-        l = gl.graph_ssl(W,centroids,range(k),method='poisson')
-        #Compute accuracy
-        acc = gl.clustering_accuracy(l,labels)   
-        if show_plot:
-            print("Accuracy=%f"%acc)
-            #Plot result (red points are labels)
-            scat1=plt.scatter(X[:,0],X[:,1], c=l)
-            scat2=plt.scatter(X[centroids,0],X[centroids,1], c='r')
-            plt.pause(0.1) #This makes matplotlib draw to the window within the loop, instead of at end
-            scat1.remove()
-            scat2.remove()
-
-        #Update means
-        if centroid_method == 'closest': 
-            m = [np.mean(X[l==j, :] ,axis=0) for j in range(k)]
-            centroids = [np.argmin(np.linalg.norm(X - m[j],axis=1)) for j in range(k)]
-        elif centroid_method == 'laplacian_eigenvector':
-            eigenvectors = np.vstack((gl.dirichlet_eigenvectors(L,indices[l != i],1)[0] for i in range(k)))
-            centroids = np.argmax(eigenvectors, axis=1)
-        elif centroid_method == 'poisson_solve':
-            centroids = [np.argmax(np.abs(gl.constrained_solve(L,indices[l != j],0, np.ones(n)))) for j in range(k)]
-        elif centroid_method == 'dijkstra':
-            centroids = [np.argmax(gl.cDijkstra(L,indices[l != j],0)) for j in range(k)]
-    if show_plot:
-            print("Accuracy=%f"%acc)
-            #Plot result (red points are labels)
-            scat1=plt.scatter(X[:,0],X[:,1], c=l)
-            scat2=plt.scatter(X[centroids,0],X[centroids,1], c='r')            
-    return l
+    run_metrics = []
+    y_preds = []
+    if not centroid_inits:
+        centroid_inits = [np.random.choice(indices,size=k,replace=False) for _ in range(num_starts)]
+    for centroids in centroid_inits:
+        # Main loop
+        for i in range(max_iter):
+            #Semi-supervised learning 
+            y_pred = gl.graph_ssl(W, centroids, np.arange(k),method='poisson')
+            centroids = update_centroids(W, L, y_pred, indices, n, k, centroid_method)
+        y_preds.append(y_pred)
+        run_metrics.append(graph_cut(L, label_matrix(y_pred)))
+    best_index = np.argmin(run_metrics)
+    return y_preds[best_index]
+            
 # Bottom up approach.
-def withinss(x):
+def withinss(x, n):
     x = np.sort(x)
     v = np.zeros(n-1,)
     for i in range(n-1):
@@ -94,26 +102,7 @@ def withinss(x):
     ind = np.argmin(v)
     threshold = x[ind] # Clustering threshold
     loss = v[ind] # Clustering loss function (goodness of fit)
-    return threshold, loss
-def withinss_cluster(x, n_clusters):
-    n = len(x)
-    x = np.sort(x)
-    k = n_clusters
-    D = np.zeros((n+1, k+1))
-    B = np.zeros((n, k))
-    mu = np.zeros(n)
-    mu[0] = x[0]
-    withinss_vals = np.zeros(n)  # withinss_vals[i] = withinss([x_1, ... , x_i])
-    for i in range(1,n):
-        mu[i] = (x[i] + (i-1)*mu[i-1])/i
-        withinss_vals[i] = withinss_vals[i-1] + ((i-1)/i)* (x[i] - mu[i-1])**2
-    for i in range(1,n+1):
-        
-        withinss_vals
-        for m in range(1, k+1):
-            vals = [D[j-1, m-1] + withinss_vals[j,i] for j in range(m,i+1)]
-            j = np.argmin(vals)
-        
+    return threshold, loss     
 def find_split(L, nodes, trials=1):
     nodes = np.asarray(nodes)
     n = nodes.shape[0]
@@ -126,34 +115,34 @@ def find_split(L, nodes, trials=1):
         # Solve the Poisson equation
         u = gl.pcg_solve(L,f,x0=None,tol=1e-10)
         assert np.sum(f) < 1e-9 # Ensure u is mean 0
-        threshold, loss = withinss(u)
+        threshold, loss = withinss(u, n)
         if loss < best_loss:
             best_loss = loss
             best_threshold = threshold
             best_u = u
-    nodes_left = np.asarray([node for node in nodes if best_u[node] <= best_threshold])
-    nodes_right = np.asarray([node for node in nodes if best_u[node] > best_threshold])
+    nodes_left = np.asarray([nodes[i] for i in range(n) if best_u[i] <= best_threshold])
+    nodes_right = np.asarray([nodes[i] for i in range(n) if best_u[i] > best_threshold])
     return nodes_left, nodes_right, best_loss     
-
-def poisson_cluster_bottom(W, n_clusters=2, split_trials = 100):
+# Bottom-up hierarchical clustering approach. Each point starts in its own cluster, and clusters are progressively merged to minimize the withinSS criterion.
+def poisson_cluster_bottom(W, L, n_clusters=2, split_trials = 10):
     n = W.shape[0]
-    L = gl.graph_laplacian(W)
-    L = L.tolil()
-    nodes = range(n)
+    nodes = np.arange(n)
     losses = {}
     splits = {} # Can make more efficient if we make splits a Binary tree?
     clusters = [nodes] # List of clusters we will consider splitting
     one_pt_clusters = []
     k = 1
+    y = np.zeros(n)
     while k < n_clusters:
-        if not clusters:
+        if clusters == []:
             raise Exception('There are insufficient data points to obtain the desired number of clusters')
         chosen_cluster = None
         best_loss = np.Inf
         for cluster in clusters:
-            if cluster not in losses:
+            if tuple(cluster) not in losses:
                 L_cluster = L[cluster, :]
-                L_cluster = L[:, cluster]
+                L_cluster = L_cluster[:, cluster]
+                assert L_cluster.shape[0] == L_cluster.shape[1]
                 left_cluster, right_cluster, loss = find_split(L_cluster, cluster, trials=split_trials)
                 losses[tuple(cluster)] = loss
                 splits[tuple(cluster)] = left_cluster, right_cluster
@@ -168,13 +157,26 @@ def poisson_cluster_bottom(W, n_clusters=2, split_trials = 100):
             else:
                 one_pt_clusters.append(item)
         k += 1
-    return one_pt_clusters+clusters, splits, losses
+    for i, cluster in enumerate(one_pt_clusters+clusters):
+        y[cluster] = i
+    return y
 
-# Return the graph-cut energy. U is an nxk matrix whose rows are the one-hot encoded labels, and L is the graph Laplacian.
+# Clustering purity measures (unsupervised)
+
+# sklearn.metrics.silhouette_score(X, labels, *, metric='euclidean', sample_size=None, random_state=None, **kwds)
+# X is the matrix of pairwise distances
+
+# Supervised clustering metrics
+# sklearn.metrics.adjusted_rand_score(labels_true, labels_pred)
+# sklearn.metrics.f1_score(y_true, y_pred, *, labels=None, pos_label=1, average='binary', sample_weight=None, zero_division='warn')
+# sklearn.metrics.normalized_mutual_info_score(labels_true, labels_pred, *, average_method='arithmetic')
+# gl.clustering_accuracy(y_true,y_pred)
+
+# Return the graph-cut energy. U is an nxk matrix whose rows are the one-hot encoded cluster labels, and L is the graph Laplacian.
 def graph_cut(L, U):
     k = U.shape[1]
+    assert type(U) == np.ndarray
     assert type(L) == scipy.sparse.csr.csr_matrix
-    assert type(U) == scipy.sparse.csr.csr_matrix
     return np.sum([U[:, i].transpose()*L*U[:, i] for i in range(k)])
 
 # Threshold the matrix U. The label decision is l(x_i) = argmax_j s_j^{-1} u(x_i)_j
@@ -193,6 +195,7 @@ def merge_matrix(k, i, j):
     A[:, j] = A[:, i] # Merge jth cluster into ith cluster, leaving row j empty
     A = np.delete(A, j, axis=0)
     return A
+# Top-down approach for clustering. We begin with one cluster containing all points, and clusters progressively split such that a criterion (graph cut, withinSS) is minimized.
 def poisson_cluster_top(W, n_clusters=2, use_cuda=False):
     n = W.shape[0]
     N = int(0.1*n)
@@ -229,16 +232,51 @@ def poisson_cluster_top(W, n_clusters=2, use_cuda=False):
         break
     return U
 
+### Experiments ###
+def experiment(X, y, W, L, centroid_methods = ['dijkstra', 'poisson_solve', 'laplacian_eigenvector'], centroid_inits=None, show_plot=False, num_starts=3, include=['poisson_kmeans']):
+    k=len(np.unique(y))
+    n = len(X)
+    indices = np.arange(n)
+    if not centroid_inits:
+        centroid_inits = [np.random.choice(indices,size=k,replace=False) for _ in range(num_starts)]
+    sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=y).set(title='Ground Truth')
+    plt.show()
+    #poisson_cluster_bottom(W, L, len(np.unique(y)))
+    # Poisson k-means
+    try:
+        y_kmeans = {method: poisson_cluster_kmeans(W, y, L, k, centroid_method=method, num_starts=num_starts, centroid_inits=centroid_inits) for method in centroid_methods}
+    except:
+        print('Encountered an error in running poisson k-means')
+        print(f'centroid inits: {centroid_inits}')
+        raise Exception('Poisson k-means encountered an error')
+    #Spectral clustering
+    y_spec = gl.spectral_cluster(W,k)
+    # Generate plots
+    sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=y_spec).set(title='Spectral Clustering')
+    plt.show()
+    for method in centroid_methods:
+        g = sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=y_kmeans[method])
+        g.set(title = f'Poisson Clustering ({method})')
+        plt.show()
+    metric_names = ['Accuracy', 'Adjusted Rand Score (0-1)', 'Normalized Mutual Information (0-1)']
+    metrics = [gl.clustering_accuracy, sklearn.metrics.adjusted_rand_score, sklearn.metrics.normalized_mutual_info_score]
+    for metric, name in zip(metrics, metric_names):
+        print(f"\n ### {name} ### \n")
+        print(f"Spectral Clustering {name}={metric(y, y_spec)}")
+        for method, y_pred in y_kmeans.items():
+            print(f"Poisson Clustering ({method}) {name}={metric(y, y_pred)}")
+
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
 if __name__ == "__main__":
-    n = 500
-    nodes = range(n)
-    
+    X, y = datasets.load_wine(return_X_y=True)
+    indices = np.arange(X.shape[0])
+    # Standardize and apply PCA as a preprocessing step
+    scaler = StandardScaler()
+    X_scale = scaler.fit_transform(X)
+    pca = PCA(n_components=X.shape[1])
+    X = pca.fit_transform(X_scale)
+    W = build_graph(X)
     L = gl.graph_laplacian(W)
-    N = 3
-    indices = np.random.choice(nodes, replace=False, size=N)
-    u, _ = gl.poisson(W,indices,range(N))
-    degrees = W.sum(axis=1)
-    class_sizes = np.diag([1,1,1])
-    #clusters, splits, losses = poisson_cluster_bottom(W, 2, split_trials=1)
-    #left, right, loss = find_split(L, nodes)
-    poisson_cluster_top(W, 2)
+    poisson_cluster_bottom(W, L, len(np.unique(y)))
